59a7045f582d75c672ed5991326679a862ade932
Ковальков Александр <akovalkoff@yandex.ru>
2022-07-13 23:55:38 +0300

    deposit amound

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int balance = 250; // баланс
-        int refil = 455; // сумма пополнения
+        int balance = 100; // баланс
+        int refil = 1500; // сумма пополнения
         int bonus = 0; // бонусный счет на данный момент
 
-        if(refil >= 100) {
+        if(refil >= 1000) {
 
             bonus = refil / 100;
             balance = balance + refil + bonus;
